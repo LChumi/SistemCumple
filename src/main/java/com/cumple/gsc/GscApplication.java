06085dcacbf482aa2d03ee85750cc1f7143b86e9
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-06-28 13:18:58 -0500

    Project initialization

---
+++ src/main/java/com/cumple/gsc/GscApplication.java
@@ -0,0 +1,13 @@
+package com.cumple.gsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GscApplication.class, args);
+	}
+
+}
