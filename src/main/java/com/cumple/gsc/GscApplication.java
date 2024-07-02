0b1ba3acb42d14757a84c3dbaa35719243e73318
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-07-02 14:52:37 -0500

    feat(swagger implementation)

--- src/main/java/com/cumple/gsc/GscApplication.java
+++ src/main/java/com/cumple/gsc/GscApplication.java
@@ -1,9 +1,12 @@
 package com.cumple.gsc;
 
+import com.cumple.gsc.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(OpenAPIConfiguration.class)
 public class GscApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/cumple/gsc/config/OpenAPIConfiguration.java
@@ -0,0 +1,24 @@
+package com.cumple.gsc.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfiguration{
+
+    @Bean
+    public OpenAPI expenseApi() {
+        return new OpenAPI()
+                .info(new Info().title("Importadora cumpleaños API")
+                        .description("API de importadora cumpleaños ASSIST WEB")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache License Version 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                ).externalDocs(new ExternalDocumentation()
+                        .description("Assist WEB")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0"));
+    }
+}
